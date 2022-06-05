b4266d9ba9b4820e25b22354c5ceac33e2dae702
rafaelacorrea <rafaela.f.c.correa@gmail.com>
2022-06-05 17:58:44 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const cliente1Nome = "Rafaela";
+const cliente1CPF = 11122233345;
+const cliente1Agencia = 1001;
+const cliente1Saldo = 0;
+
+const cliente2Nome = "Fernanda";
+const cliente2CPF = 11122233345;
+const cliente2Agencia = 1001;
+const cliente2Saldo = 0;
+
+const cliente3Nome = "Frank";
+const cliente3CPF = 11122233345;
+const cliente3Agencia = 1001;
+const cliente3Saldo = 0;
+
+console.log(cliente1Nome)
+console.log(cliente2Nome)
+console.log(cliente3Nome)
